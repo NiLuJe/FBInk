7ce7aae73ccc048348c17b8cc18302afcd5134e1
NiLuJe <ninuje@gmail.com>
2018-06-06 22:53:02 +0200

    Update header
    Add license

--- fbink.c
+++ fbink.c
@@ -1,18 +1,29 @@
 /*
- * fbtestfnt.c
- *
- * http://raspberrycompote.blogspot.com/2014/04/low-level-graphics-on-raspberry-pi-text.html
- *
- * Original work by J-P Rosti (a.k.a -rst- and 'Raspberry Compote')
- *
- * Licensed under the Creative Commons Attribution 3.0 Unported License
- * (http://creativecommons.org/licenses/by/3.0/deed.en_US)
- *
- * Distributed in the hope that this will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- */
+	FBInk: FrameBuffer eInker, a tool to print strings on eInk devices (Kobo/Kindle)
+	Copyright (C) 2018 NiLuJe <ninuje@gmail.com>
+
+	Linux framebuffer routines based on: fbtestfnt.c & fbtest6.c, from
+	http://raspberrycompote.blogspot.com/2014/04/low-level-graphics-on-raspberry-pi-text.html &
+	https://raspberrycompote.blogspot.com/2013/03/low-level-graphics-on-raspberry-pi-part_8.html
+	Original works by J-P Rosti (a.k.a -rst- and 'Raspberry Compote'),
+	Licensed under the Creative Commons Attribution 3.0 Unported License
+	(http://creativecommons.org/licenses/by/3.0/deed.en_US)
+
+	----
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <fcntl.h>
 #include <getopt.h>
@@ -629,9 +640,7 @@ int
 }
 
 /*
- * TODO: Header (+ _internal.h)
  * TODO: Makefile
- * TODO: License
  * TODO: DOC
  * TODO: Library (thread safety: fbinlk_init to setup globals? /!\ fb fd?)
  * TODO: waveform mode user-selection? -w
