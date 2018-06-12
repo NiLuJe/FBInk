66a44610cef56e4406d0b7379de211409d7bac5f
NiLuJe <ninuje@gmail.com>
2018-06-12 20:30:27 +0200

    Trying very hard not to try this with ALL THE FONTS! :D

--- fbink_internal.h
+++ fbink_internal.h
@@ -69,6 +69,11 @@
 #else
 #	include "eink/mxcfb-kobo.h"
 #endif
+
+// NOTE: This is from https://github.com/dhepper/font8x8
+//       See also https://github.com/achilikin/bdfe
+//       & https://unix.stackexchange.com/q/119236
+//       for fun ways to add new glyphs or just change the font ;).
 #include "font8x8/font8x8_basic.h"
 #include "font8x8/font8x8_block.h"
 #include "font8x8/font8x8_box.h"
