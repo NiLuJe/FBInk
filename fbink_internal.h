e928fb4b0ed77e553663e8fa1b14fb22d7a0463c
NiLuJe <ninuje@gmail.com>
2018-06-30 22:22:42 +0200

    Bump to v0.9.12

--- fbink_internal.h
+++ fbink_internal.h
@@ -100,7 +100,7 @@
 
 // Fallback version tag...
 #ifndef FBINK_VERSION
-#	define FBINK_VERSION "v0.9.10"
+#	define FBINK_VERSION "v0.9.12"
 #endif
 
 // NOTE: Some of our ifdef combinations may cause a small number of function arguments to become unused...
