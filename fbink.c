163d31e52220838c8d976bf80920ea06979aedf4
NiLuJe <ninuje@gmail.com>
2018-07-19 00:06:32 +0200

    Resolve the dilemma by enabling this in Debug builds only...
    Instead of manually having to flip the comments when I need it...

--- fbink.c
+++ fbink.c
@@ -207,13 +207,14 @@ static void
 	//       when we're padding and centering, the final whitespace of right-padding will have its last
 	//       few pixels (the exact amount being half of the dead zone width) pushed off-screen...
 	if (coords.x >= vinfo.xres || coords.y >= vinfo.yres) {
-		/*
+#ifdef DEBUG
+		// NOTE: This is only enabled in Debug builds because it can be pretty verbose...
 		LOG("Discarding off-screen pixel @ (%hu, %hu) (out of %ux%u bounds)",
 		    coords.x,
 		    coords.y,
 		    vinfo.xres,
 		    vinfo.yres);
-		*/
+#endif
 		return;
 	}
 
