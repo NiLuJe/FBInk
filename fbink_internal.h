dd226d7dce7671205be894f3270db40a37c47161
NiLuJe <ninuje@gmail.com>
2018-07-21 19:20:46 +0200

    Bump to v1.2.5

--- fbink_internal.h
+++ fbink_internal.h
@@ -114,7 +114,7 @@
 
 // Fallback version tag...
 #ifndef FBINK_VERSION
-#	define FBINK_VERSION "v1.2.0"
+#	define FBINK_VERSION "v1.2.5"
 #endif
 
 // NOTE: Some of our ifdef combinations may cause a small number of function arguments to become unused...
