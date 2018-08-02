b80c5dd97851902fabcd248a7eccb3bf8928b96d
NiLuJe <ninuje@gmail.com>
2018-08-02 20:29:58 +0200

    More doc tweaks

--- fbink.h
+++ fbink.h
@@ -111,7 +111,7 @@ FBINK_API int fbink_init(int, const FBInkConfig*);
 //       c.f., my rant about Kobo's broken libc in fbink_internal.h for more details behind this choice.
 //       Since any decent system of the last decade should default to UTF-8, that should be pretty much transparent...
 // Returns the amount of lines printed on success.
-// Arg 1: fbfd, if it's -1, the fb is opened for the duration of this call
+// Arg 1: fbfd, if it's -1, the fb is opened & mmap'ed for the duration of this call
 // Arg 2: UTF-8 encoded string to print
 // Arg 3: pointer to an FBInkConfig struct
 FBINK_API int fbink_print(int, const char*, const FBInkConfig*);
@@ -122,7 +122,7 @@ FBINK_API int fbink_print(int, const char*, const FBInkConfig*);
 FBINK_API int fbink_printf(int, const FBInkConfig*, const char*, ...) __attribute__((format(printf, 3, 4)));
 
 // And a simple wrapper around the internal refresh, without having to include mxcfb headers
-// Arg 1: fbfd, if it's -1, the fb is opened for the duration of this call
+// Arg 1: fbfd, if it's -1, the fb is opened & mmap'ed for the duration of this call
 // Arg 2: top field of an mxcfb rectangle
 // Arg 3: left field of an mxcfb rectangle
 // Arg 4: width field of an mxcfb rectangle
@@ -139,7 +139,7 @@ FBINK_API bool fbink_is_fb_quirky(void);
 
 // Print an image on screen
 // Returns -(ENOSYS) when image support is disabled (MINIMAL build)
-// Arg 1: fbfd, if it's -1, the fb is opened for the duration of this call
+// Arg 1: fbfd, if it's -1, the fb is opened & mmap'ed for the duration of this call
 // Arg 2: path to the image file
 // Arg 3: target coordinates, x
 // Arg 4: target coordinates, y
