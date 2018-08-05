272430c04fa7f4e15ea2a6aa254039b09eb1af75
NiLuJe <ninuje@gmail.com>
2018-08-05 18:46:16 +0200

    Make that 5 seconds, to be on the safe side...

--- fbink_button_scan.c
+++ fbink_button_scan.c
@@ -56,8 +56,8 @@ static int
 	// Assume success until shit happens :)
 	int rv = EXIT_SUCCESS;
 
-	// Check if screen content changed 4s later, as a poor man's way of checking if the tap was successful...
-	nanosleep((const struct timespec[]){ { 4, 0 } }, NULL);
+	// Check if screen content changed 5s later, as a poor man's way of checking if the tap was successful...
+	nanosleep((const struct timespec[]){ { 5, 0 } }, NULL);
 
 	// Neuter logging for this pass...
 	bool orig_verbose = g_isVerbose;
