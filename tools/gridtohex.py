7000621499313fb35810b9a5388b178454c1a708
NiLuJe <ninuje@gmail.com>
2018-08-13 02:27:47 +0200

    Add a script to convert an ASCII-art grid to the Hex format
    And do it for PoP's block font

---
+++ tools/gridtohex.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#
+# Quick'n dirty script to convert an ASCII-art grid into Unifont's hex format
+# Assumes a 32x32 grid
+#
+##
+
+import re
+import sys
+
+# Convert a binary representation (in string form) to hex
+# c.f., https://stackoverflow.com/q/2072351
+def bin2hex(s):
+	return '%0*X' % ((len(s) + 3) // 4, int(s, 2))
+
+fontfile = "../fonts/block.txt"
+fontname = "block"
+
+cp = 0x0
+prevcp = 0x0
+fmt = re.compile(r"([\.#]{32})")
+cpre = re.compile(r"(U\+)([0-9a-fA-F]{4})(.*?)")
+
+with open(fontfile, "r") as f:
+	for line in f:
+		m = cpre.match(line)
+		if m:
+			cp = m.group(2)
+			print("")
+			print("{}:".format(cp), end='')
+		else:
+			m = fmt.match(line)
+			if m:
+				row = m.group(1)
+				# ASCII art to binary
+				row = row.replace('.', '0')
+				row = row.replace('#', '1')
+				print("{}".format(bin2hex(row)), end='')
+print("")
