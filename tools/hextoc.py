9f71f03ee562871b339e4d62b1bc029d938eec8a
NiLuJe <ninuje@gmail.com>
2018-11-26 22:12:37 +0100

    Suppress some more warnings

--- tools/hextoc.py
+++ tools/hextoc.py
@@ -99,13 +99,13 @@ with open(fontfile, "r") as f:
 					print("}}; // {}".format(blockcount))
 					print("")
 					if blocknum == 1:
-						if fontwidth <= 8:
+						if fontwidth <= 8: # lgtm[py/unreachable-statement]
 							eprint("static const unsigned char*")
-						elif fontwidth <= 16:
+						elif fontwidth <= 16: # lgtm[py/unreachable-statement]
 							eprint("static const uint16_t*")
-						elif fontwidth <= 32:
+						elif fontwidth <= 32: # lgtm[py/unreachable-statement]
 							eprint("static const uint32_t*")
-						elif fontwidth <= 64:
+						elif fontwidth <= 64: # lgtm[py/unreachable-statement]
 							eprint("static const uint64_t*")
 						else:
 							exit(-1)
@@ -123,29 +123,29 @@ with open(fontfile, "r") as f:
 				blocknum += 1
 				blockcount = 1
 				blockcp = cp
-				if fontwidth <= 8:
+				if fontwidth <= 8: # lgtm[py/unreachable-statement]
 					print("static const unsigned char {}_block{}[][{}] = {{".format(fontname, blocknum, fontheight))
-				elif fontwidth <= 16:
+				elif fontwidth <= 16: # lgtm[py/unreachable-statement]
 					print("static const uint16_t {}_block{}[][{}] = {{".format(fontname, blocknum, fontheight))
-				elif fontwidth <= 32:
+				elif fontwidth <= 32: # lgtm[py/unreachable-statement]
 					print("static const uint32_t {}_block{}[][{}] = {{".format(fontname, blocknum, fontheight))
-				elif fontwidth <= 64:
+				elif fontwidth <= 64: # lgtm[py/unreachable-statement]
 					print("static const uint64_t {}_block{}[][{}] = {{".format(fontname, blocknum, fontheight))
 				else:
 					exit(-1)
 
 			hcp = int(cp, base=16)
 			print(u"\t{", end='')
-			if fontwidth <= 8:
+			if fontwidth <= 8: # lgtm[py/unreachable-statement]
 				for i in range(fontheight):
 					print(u" {:#04x}".format(hex2f8(m.group(i+2))), end='' if i+1 == fontheight else ',')
-			elif fontwidth <= 16:
+			elif fontwidth <= 16: # lgtm[py/unreachable-statement]
 				for i in range(fontheight):
 					print(u" {:#06x}".format(hex2f16(m.group(i+2))), end='' if i+1 == fontheight else ',')
-			elif fontwidth <= 32:
+			elif fontwidth <= 32: # lgtm[py/unreachable-statement]
 				for i in range(fontheight):
 					print(u" {:#010x}".format(hex2f32(m.group(i+2))), end='' if i+1 == fontheight else ',')
-			elif fontwidth <= 64:
+			elif fontwidth <= 64: # lgtm[py/unreachable-statement]
 				for i in range(fontheight):
 					print(u" {:#018x}".format(hex2f64(m.group(i+2))), end='' if i+1 == fontheight else ',')
 			else:
