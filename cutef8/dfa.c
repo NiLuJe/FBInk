9815bd667143bdbae356c16965393b4cd6808bc6
NiLuJe <ninuje@gmail.com>
2019-01-17 04:07:44 +0100

    Well, that settles that ;).

--- cutef8/dfa.c
+++ cutef8/dfa.c
@@ -23,6 +23,7 @@ static const uint8_t utf8d[] = {
 // NOTE: See https://lists.w3.org/Archives/Public/www-archive/2013Apr/0041.html
 //       for an explanation why the original code used uint32_t for byte (and in turn type) ;).
 //       FWIW, I'm not seeing a noticeable shift in performance on ARM with my GCC 7 TCs.
+//       More importantly, the original version is crashy on x86_64 (GCC 8.2)...
 inline static uint8_t
     decode(uint8_t* state, uint32_t* codep, uint8_t byte)
 {
