b814be72f241d2b8cd8f2d0ae33e477e55941290
NiLuJe <ninuje@gmail.com>
2019-01-18 03:17:58 +0100

    Yeah, the ladder is sparse, can't elide *any* bounds check...

--- fbink.c
+++ fbink.c
@@ -573,15 +573,15 @@ static const unsigned char*
 	// Get the bitmap for the character mapped to that Unicode codepoint
 	if (codepoint <= 0x7F) {
 		return font8x8_basic[codepoint];
-	} else if (codepoint >= 0x80 && codepoint <= 0x9F) {
+	} else if (codepoint >= 0x80 && codepoint <= 0x9F) {    // lgtm [cpp/constant-comparison]
 		return font8x8_control[codepoint - 0x80];
-	} else if (codepoint >= 0xA0 && codepoint <= 0xFF) {
+	} else if (codepoint >= 0xA0 && codepoint <= 0xFF) {    // lgtm [cpp/constant-comparison]
 		return font8x8_ext_latin[codepoint - 0xA0];
 	} else if (codepoint >= 0x390 && codepoint <= 0x3C9) {
 		return font8x8_greek[codepoint - 0x390];
 	} else if (codepoint >= 0x2500 && codepoint <= 0x257F) {
 		return font8x8_box[codepoint - 0x2500];
-	} else if (codepoint >= 0x2580 && codepoint <= 0x259F) {
+	} else if (codepoint >= 0x2580 && codepoint <= 0x259F) {    // lgtm [cpp/constant-comparison]
 		return font8x8_block[codepoint - 0x2580];
 	} else if (codepoint >= 0x3040 && codepoint <= 0x309F) {
 		return font8x8_hiragana[codepoint - 0x3040];
