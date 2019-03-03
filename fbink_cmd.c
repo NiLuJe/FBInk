85c7e4c8e72fb41fe8aa44b9f3c2b1d9368ef0c3
NiLuJe <ninuje@gmail.com>
2019-03-03 13:11:00 +0100

    Okay, slightly better fix for #32
    Less side-effects.
    This only prohibits "follow" use-cases over non-interactive SSH
    sessions instead of everywhere.

--- fbink_cmd.c
+++ fbink_cmd.c
@@ -1240,33 +1240,54 @@ int
 				size_t  len  = 0;
 				ssize_t nread;
 				int     linecnt = -1;
-				// Check that we have stuff to read from stdin first,
+
+				// If we're being run from a non-interactive SSH session, abort early if stdin is empty,
 				// as we do *NOT* want an interactive behavior here (c.f., #32).
-				int n;
-				if (ioctl(fileno(stdin), FIONREAD, &n) == 0 && n > 0) {
-					// Did we ask for OT rendering?
-					if (is_truetype) {
-						load_ot_fonts(
-						    reg_ot_file, bd_ot_file, it_ot_file, bdit_ot_file, &fbink_cfg);
-						while ((nread = getline(&line, &len, stdin)) != -1) {
-							if ((linecnt = fbink_print_ot(
-								 fbfd, line, &ot_config, &fbink_cfg)) < 0) {
-								fprintf(stderr, "Failed to print that string!\n");
-								rv = ERRCODE(EXIT_FAILURE);
-							}
-							ot_config.margins.top = (short int) linecnt;
+				// We have to jump through a few extra hoops,
+				// because we *do* want to allow blocking behavior in general, as it can be useful,
+				// for instance when piping the output of a tail -f like tool.
+				// NOTE: The obvious downside is that, over SSH,
+				//       this specific "follow" use-case will only reliably work from an *interactive* SSH session!
+				if (getenv("SSH_CLIENT") != NULL && getenv("SSH_TTY") == NULL) {
+					// Okay, we're a non-interactive SSH session...
+					int n;
+					if (ioctl(fileno(stdin), FIONREAD, &n) == 0 && n == 0) {
+						// Okay, stdin was empty *at call time*,
+						// don't even try to wait for us to have someting to read from it...
+						fprintf(
+						    stderr,
+						    "This is a non-interactive SSH session and stdin is *currently* empty, enforcing non-blocking behavior by aborting early!\n");
+						// We're not printing anything, so, do the usual clear or help dance.
+						if (fbink_cfg.is_cleared) {
+							rv = fbink_cls(fbfd, &fbink_cfg);
+						} else {
+							show_helpmsg();
 						}
-					} else {
-						while ((nread = getline(&line, &len, stdin)) != -1) {
-							if ((linecnt = fbink_print(fbfd, line, &fbink_cfg)) < 0) {
-								fprintf(stderr, "Failed to print that string!\n");
-								rv = ERRCODE(EXIT_FAILURE);
-							}
-							fbink_cfg.row = (short int) (fbink_cfg.row + linecnt);
-						}
+						// And abort early ;).
+						goto cleanup;
 					}
-					free(line);
 				}
+
+				// Did we ask for OT rendering?
+				if (is_truetype) {
+					load_ot_fonts(reg_ot_file, bd_ot_file, it_ot_file, bdit_ot_file, &fbink_cfg);
+					while ((nread = getline(&line, &len, stdin)) != -1) {
+						if ((linecnt = fbink_print_ot(fbfd, line, &ot_config, &fbink_cfg)) < 0) {
+							fprintf(stderr, "Failed to print that string!\n");
+							rv = ERRCODE(EXIT_FAILURE);
+						}
+						ot_config.margins.top = (short int) linecnt;
+					}
+				} else {
+					while ((nread = getline(&line, &len, stdin)) != -1) {
+						if ((linecnt = fbink_print(fbfd, line, &fbink_cfg)) < 0) {
+							fprintf(stderr, "Failed to print that string!\n");
+							rv = ERRCODE(EXIT_FAILURE);
+						}
+						fbink_cfg.row = (short int) (fbink_cfg.row + linecnt);
+					}
+				}
+				free(line);
 
 				// If nothing was read, show the help
 				if (linecnt == -1) {
