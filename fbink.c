f68e0feeec4a19b87c71ffde167a15ca1a3d841d
NiLuJe <ninuje@gmail.com>
2019-03-27 19:46:20 +0100

    Initial region restore...

--- fbink.c
+++ fbink.c
@@ -6084,6 +6084,19 @@ int
 		// Full dump, easy enough
 		memcpy(fbPtr, dump->data, fInfo.line_length * vInfo.yres);
 		fullscreen_region(&region);
+	} else {
+		// Region dump, restore line by line
+		// FIXME: 4bpp?
+		uint8_t bpp = dump->bpp / 8U;
+		for (unsigned short int j = dump->y, l = 0U; j < dump->y + dump->h; j++, l++) {
+			size_t fb_offset   = (uint32_t)(dump->x * bpp) + (j * fInfo.line_length);
+			size_t dump_offset = l * (dump->w * bpp);
+			memcpy(fbPtr + fb_offset, dump->data + dump_offset, dump->w * bpp);
+		}
+		region.left   = dump->x;
+		region.top    = dump->y;
+		region.width  = dump->w;
+		region.height = dump->h;
 	}
 
 	// And now, we can refresh the screen
