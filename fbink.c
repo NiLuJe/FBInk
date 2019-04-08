679754e4fdddba05787d683e33c8d1805cbebe67
NiLuJe <ninuje@gmail.com>
2019-04-09 01:59:39 +0200

    Update CREDITS & attribution header.

--- fbink.c
+++ fbink.c
@@ -2,12 +2,18 @@
 	FBInk: FrameBuffer eInker, a tool to print text & images on eInk devices (Kobo/Kindle)
 	Copyright (C) 2018-2019 NiLuJe <ninuje@gmail.com>
 
-	Some (8/16/24/32bpp) Linux framebuffer writing routines based on: fbtestfnt.c & fbtest6.c, from
+	Some (the 8/16/24/32bpp put_pixel_* variants) Linux framebuffer writing routines based on: fbtestfnt.c & fbtest6.c, from
 	http://raspberrycompote.blogspot.com/2014/04/low-level-graphics-on-raspberry-pi-text.html &
 	https://raspberrycompote.blogspot.com/2013/03/low-level-graphics-on-raspberry-pi-part_8.html
 	Original works by J-P Rosti (a.k.a -rst- and 'Raspberry Compote'),
 	Licensed under the Creative Commons Attribution 3.0 Unported License
 	(http://creativecommons.org/licenses/by/3.0/deed.en_US)
+
+	Ordered dithering routine (dither_o8x8) completely based on ImageMagick's implementation,
+	(OrderedDitherImage @ https://github.com/ImageMagick/ImageMagick/blob/master/MagickCore/threshold.c),
+	Copyright 1999-2019 ImageMagick Studio LLC,
+	Licensed under the ImageMagick License,
+	(https://imagemagick.org/script/license.php)
 
 	----
 
