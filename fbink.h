8d8c34c7641d07fe7c0694c32590cdba8a7eb33d
NiLuJe <ninuje@gmail.com>
2019-05-25 17:28:54 +0200

    Amend that comment ;).
    pickel uses a sane rotation on Mk. 7, for instance.

--- fbink.h
+++ fbink.h
@@ -610,7 +610,7 @@ FBINK_API int fbink_restore(int fbfd, const FBInkConfig* restrict fbink_cfg, con
 //       this will get updated regardless of no_refresh,
 //       and will ignore what is_flashing might do to make the refresh region fullscreen.
 //       i.e., it corresponds to what's drawn to the fb, not necessarily to what's refreshed on screen.
-// NOTE: On devices where we may fudge the coordinates to account for broken rotation (i.e., Kobos @ 16bpp),
+// NOTE: On devices where we may fudge the coordinates to account for broken rotation (i.e., most Kobos @ 16bpp),
 //       these are the *rotated* coordinates!
 //       i.e., they *will* match with what we actually send to mxcfb (and where we actually drew on the fb)!
 FBINK_API FBInkRect fbink_get_last_rect(void);
