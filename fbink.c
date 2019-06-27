e43c9b202786c3caaf1f32eb8938c9159da2628e
NiLuJe <ninuje@gmail.com>
2019-06-27 23:52:58 +0200

    Minor comment tweaks

--- fbink.c
+++ fbink.c
@@ -3042,7 +3042,7 @@ int
 				// even if the reflowing changes that'll cause mean we might not end up using it.
 				lines++;
 				// Don't decrement the byte index, we want to print the LF,
-				// (which we'll replace with a blank, to account for fonts with a visible LF glyph),
+				// (which we'll replace with a blank space, to account for fonts with a visible LF glyph),
 				// mostly to make padding look nicer,
 				// but also so that line_bytes matches line_len ;).
 				// And finally, as we've explained earlier, trim line_len to where we stopped.
@@ -3150,11 +3150,12 @@ int
 		//       because I'm fairly confident that truncation is not a risk here...
 		LOG("snprintf wrote %d bytes", bytes_printed);
 
-		// NOTE: If we caught a LF, replace it with a blank to make it behave with fonts that have a visible LF glyph...
+		// NOTE: If we caught a LF, replace it with a space to make it behave with fonts that have a visible LF glyph...
 		if (caught_lf) {
 			// We can't rely on line_bytes as the exact position of the LF, since padding may have moved it...
 			char* lf = strrchr(line, 0x0A);
 			if (lf) {
+				// LF -> space
 				*lf = 0x20;
 			}
 		}
