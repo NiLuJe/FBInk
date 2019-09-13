049c0b65cbd51c8cb81f6d89e539d6460851b335
NiLuJe <ninuje@gmail.com>
2019-09-13 16:13:43 +0200

    Okay, first attempt
    It segfaults ;p.

--- utils/doom.c
+++ utils/doom.c
@@ -26,6 +26,7 @@
 
 #include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 // I feel dirty.
 #include "../fbink.c"
 
@@ -198,6 +199,65 @@ static bool
 	return true;
 }
 
+// Shiny DOOM Fire effect
+// c.f., http://fabiensanglard.net/doom_fire_psx/index.html
+//     & https://github.com/fabiensanglard/DoomFirePSX/blob/master/flames.html
+//     & https://github.com/cylgom/ly/blob/master/src/draw.c
+static const uint8_t fire_colors[][3] = {
+	{ 0x07, 0x07, 0x07 }, { 0x1F, 0x07, 0x07 }, { 0x2F, 0x0F, 0x07 }, { 0x47, 0x0F, 0x07 }, { 0x57, 0x17, 0x07 },
+	{ 0x67, 0x1F, 0x07 }, { 0x77, 0x1F, 0x07 }, { 0x8F, 0x27, 0x07 }, { 0x9F, 0x2F, 0x07 }, { 0xAF, 0x3F, 0x07 },
+	{ 0xBF, 0x47, 0x07 }, { 0xC7, 0x47, 0x07 }, { 0xDF, 0x4F, 0x07 }, { 0xDF, 0x57, 0x07 }, { 0xDF, 0x57, 0x07 },
+	{ 0xD7, 0x5F, 0x07 }, { 0xD7, 0x5F, 0x07 }, { 0xD7, 0x67, 0x0F }, { 0xCF, 0x6F, 0x0F }, { 0xCF, 0x77, 0x0F },
+	{ 0xCF, 0x7F, 0x0F }, { 0xCF, 0x87, 0x17 }, { 0xC7, 0x87, 0x17 }, { 0xC7, 0x8F, 0x17 }, { 0xC7, 0x97, 0x1F },
+	{ 0xBF, 0x9F, 0x1F }, { 0xBF, 0x9F, 0x1F }, { 0xBF, 0xA7, 0x27 }, { 0xBF, 0xA7, 0x27 }, { 0xBF, 0xAF, 0x2F },
+	{ 0xB7, 0xAF, 0x2F }, { 0xB7, 0xB7, 0x2F }, { 0xB7, 0xB7, 0x37 }, { 0xCF, 0xCF, 0x6F }, { 0xDF, 0xDF, 0x9F },
+	{ 0xEF, 0xEF, 0xC7 }, { 0xFF, 0xFF, 0xFF },
+};
+
+uint8_t palette[sizeof(fire_colors) / sizeof(*fire_colors)];
+
+void
+    spread_fire(size_t offset)
+{
+	uint8_t pixel = *((unsigned char*) (fbPtr + offset));
+	if (pixel == palette[0U]) {
+		*((unsigned char*) (fbPtr + offset - viewWidth)) = palette[0U];
+	} else {
+		size_t random                                 = (rand() * 3U) & 3;
+		size_t dst                                    = offset - random + 1U;
+		*((unsigned char*) (fbPtr + dst - viewWidth)) = pixel - (random & 1);
+	}
+}
+
+void
+    do_fire(void)
+{
+	// Burn bay, burn!
+	for (uint32_t x = 0U; x < viewWidth; x++) {
+		for (uint32_t y = 1U + (viewVertOrigin - viewVertOffset);
+		     y < viewHeight + (viewVertOrigin - viewVertOffset);
+		     y++) {
+			spread_fire(y * viewWidth + x);
+		}
+	}
+}
+
+void
+    setup_fire(void)
+{
+	// Convert the palette to Grayscale
+	for (uint8_t i = 0U; i < sizeof(palette); i++) {
+		palette[i] = stbi__compute_y(fire_colors[i][0], fire_colors[i][1], fire_colors[i][2]);
+	}
+
+	// Fill the whole screen w/ color 0
+	memset(fbPtr, palette[0U], fInfo.smem_len);
+
+	// Set the bottom line to the final color
+	const FBInkPixel px = { .gray8 = palette[sizeof(palette) - 1U] };
+	fill_rect(0U, viewHeight + (viewVertOrigin - viewVertOffset) - 1U, viewWidth, 1U, &px);
+}
+
 int
     main(int argc, char* argv[])
 {
@@ -207,15 +267,14 @@ int
 
 	int                        opt;
 	int                        opt_index;
-	static const struct option opts[] = {
-		{ "help", no_argument, NULL, 'h' },
-		{ "verbose", no_argument, NULL, 'v' },     { "quiet", no_argument, NULL, 'q' },
-		{ NULL, 0, NULL, 0 }
-	};
+	static const struct option opts[] = { { "help", no_argument, NULL, 'h' },
+					      { "verbose", no_argument, NULL, 'v' },
+					      { "quiet", no_argument, NULL, 'q' },
+					      { NULL, 0, NULL, 0 } };
 
-	uint32_t req_bpp     = 8U;
-	int8_t   req_rota    = -1;
-	bool     errfnd      = false;
+	uint32_t req_bpp  = 8U;
+	int8_t   req_rota = -1;
+	bool     errfnd   = false;
 
 	while ((opt = getopt_long(argc, argv, "hvq", opts, &opt_index)) != -1) {
 		switch (opt) {
@@ -329,7 +388,13 @@ int
 		goto cleanup;
 	}
 
-	// TODO: Doom!
+	FBInkConfig fbink_cfg = { 0U };
+	// Fire!
+	setup_fire();
+	while (true) {
+		do_fire();
+		fbink_refresh(fbfd, 0U, 0U, 0U, 0U, EPDC_FLAG_USE_DITHERING_PASSTHROUGH, &fbink_cfg);
+	}
 
 cleanup:
 	if (fbink_close(fbfd) == ERRCODE(EXIT_FAILURE)) {
