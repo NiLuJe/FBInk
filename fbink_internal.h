b12b85a37219bade75fdbe2fd665af2f7d925d62
NiLuJe <ninuje@gmail.com>
2019-11-23 00:33:18 +0100

    Bump to v1.20.3

--- fbink_internal.h
+++ fbink_internal.h
@@ -233,7 +233,7 @@
 
 // Fallback version tag...
 #ifndef FBINK_VERSION
-#	define FBINK_FALLBACK_VERSION "v1.20.2-git"
+#	define FBINK_FALLBACK_VERSION "v1.20.3-git"
 #	ifdef FBINK_FOR_KINDLE
 #		define FBINK_VERSION FBINK_FALLBACK_VERSION " for Kindle"
 #	else
