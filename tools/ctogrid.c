8fb4c6553a2c5afe58a64f29fd75cbeaacb2264c
NiLuJe <ninuje@gmail.com>
2020-04-17 02:04:55 +0200

    Clang-format pass

--- tools/ctogrid.c
+++ tools/ctogrid.c
@@ -1,48 +1,54 @@
 // c.f., ../font8x8/render.c
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "../fonts/block.h"
 
-void usage(char *exec) {
-    printf("Usage: %s <char_code>\n", exec);
-    printf("       <char_code> Decimal character code between 20 and 255\n");
+void
+    usage(char* exec)
+{
+	printf("Usage: %s <char_code>\n", exec);
+	printf("       <char_code> Decimal character code between 20 and 255\n");
 }
 
-void render(const uint32_t *bitmap) {
-    bool set;
-    for (uint8_t x=0U; x < 32; x++) {
-        for (uint8_t y=0U; y < 32; y++) {
-        // NOTE: For the non-mirrored Unifont hex format, access rows in reverse order
-        //for (int8_t y=32-1; y >= 0; y--) {
-            set = bitmap[x] & 1U << y;
-            printf("%c", set ? '#' : '.');
-        }
-        printf("\n");
-    }
+void
+    render(const uint32_t* bitmap)
+{
+	bool set;
+	for (uint8_t x = 0U; x < 32; x++) {
+		for (uint8_t y = 0U; y < 32; y++) {
+			// NOTE: For the non-mirrored Unifont hex format, access rows in reverse order
+			//for (int8_t y=32-1; y >= 0; y--) {
+			set = bitmap[x] & 1U << y;
+			printf("%c", set ? '#' : '.');
+		}
+		printf("\n");
+	}
 }
 
-int main(int argc, char **argv) {
-    short int ord;
-    if (argc != 2) {
-        usage(argv[0]);
-        return 1;
-    }
-    ord = (short int) atoi(argv[1]);
-    // Try reading it as a char?
-    if (ord == 0) {
-        ord = argv[1][0];
-    }
-    if (ord > 255 || ord < 20) {
-        fprintf(stderr, "%hd is OOR\n", ord);
-        usage(argv[0]);
-        return 2;
-    }
-    // NOTE: First char is 0x20, convert from ASCII code to array index
-    const uint32_t *bitmap = block_block1[ord - 0x20];
+int
+    main(int argc, char** argv)
+{
+	short int ord;
+	if (argc != 2) {
+		usage(argv[0]);
+		return 1;
+	}
+	ord = (short int) atoi(argv[1]);
+	// Try reading it as a char?
+	if (ord == 0) {
+		ord = argv[1][0];
+	}
+	if (ord > 255 || ord < 20) {
+		fprintf(stderr, "%hd is OOR\n", ord);
+		usage(argv[0]);
+		return 2;
+	}
+	// NOTE: First char is 0x20, convert from ASCII code to array index
+	const uint32_t* bitmap = block_block1[ord - 0x20];
 
-    render(bitmap);
-    return 0;
+	render(bitmap);
+	return 0;
 }
--- tools/unibdf2hex.c
+++ tools/unibdf2hex.c
@@ -26,67 +26,70 @@
 #include <string.h>
 
 #define UNISTART 0x0000
-#define UNISTOP 0xFFFF
+#define UNISTOP  0xFFFF
 
 #define MAXBUF 256
 
-
 int
-main()
+    main()
 {
-   int i;
-   int digitsout;  /* how many hex digits we output in a bitmap */
-   int thispoint;
-   char inbuf[MAXBUF];
-   int bbxx, bbxy, bbxxoff, bbxyoff;
+	int  i;
+	int  digitsout; /* how many hex digits we output in a bitmap */
+	int  thispoint;
+	char inbuf[MAXBUF];
+	int  bbxx, bbxy, bbxxoff, bbxyoff;
 
-   int descent=6; /* font descent wrt baseline */
-   int startrow;  /* row to start glyph        */
-   unsigned rowout;
+	int      descent = 6; /* font descent wrt baseline */
+	int      startrow;    /* row to start glyph        */
+	unsigned rowout;
 
-   while (fgets (inbuf, MAXBUF - 1, stdin) != NULL) {
-      if (strncmp (inbuf, "ENCODING ", 9) == 0) {
-         sscanf (&inbuf[9], "%d", &thispoint); /* get code point */
-         /*
-            If we want this code point, get the BBX (bounding box) and
-            BITMAP information.
-         */
-         if (1)   // NOTE: Squished cp range check (might want to leave a thispoint <= UNISTOP?).
-            {
-            while (fgets (inbuf, MAXBUF - 1, stdin) != NULL &&
-                   strncmp (inbuf, "BBX ", 4) != 0); /* find bounding box */
+	while (fgets(inbuf, MAXBUF - 1, stdin) != NULL) {
+		if (strncmp(inbuf, "ENCODING ", 9) == 0) {
+			sscanf(&inbuf[9], "%d", &thispoint); /* get code point */
+			/* If we want this code point, get the BBX (bounding box) and BITMAP information. */
+			if (1)    // NOTE: Squished cp range check (might want to leave a thispoint <= UNISTOP?).
+			{
+				while (fgets(inbuf, MAXBUF - 1, stdin) != NULL && strncmp(inbuf, "BBX ", 4) != 0) {
+					/* find bounding box */
+					;
+				}
 
-            sscanf (&inbuf[4], "%d %d %d %d", &bbxx, &bbxy, &bbxxoff, &bbxyoff);
-            while (fgets (inbuf, MAXBUF - 1, stdin) != NULL &&
-                   strncmp (inbuf, "BITMAP", 6) != 0); /* find bitmap start */
-            fprintf (stdout, "%04X:", thispoint);
-            digitsout = 0;
-            /* Print initial blank rows */
-            startrow = descent + bbxyoff + bbxy;
+				sscanf(&inbuf[4], "%d %d %d %d", &bbxx, &bbxy, &bbxxoff, &bbxyoff);
+				while (fgets(inbuf, MAXBUF - 1, stdin) != NULL && strncmp(inbuf, "BITMAP", 6) != 0) {
+					/* find bitmap start */
+					;
+				}
+				fprintf(stdout, "%04X:", thispoint);
+				digitsout = 0;
+				/* Print initial blank rows */
+				startrow = descent + bbxyoff + bbxy;
 
-            /* Force everything to 16 pixels wide */
-            for (i = 16; i > startrow; i--) {
-               fprintf (stdout,"0000");
-               digitsout += 4;
-            }
-            while (fgets (inbuf, MAXBUF - 1, stdin) != NULL &&
-                   strncmp (inbuf, "END", 3) != 0) { /* copy bitmap until END */
-               sscanf (inbuf, "%X", &rowout);
-               /* Now force glyph to a 16x16 grid even if they'd fit in 8x16 */
-               if (bbxx <= 8) rowout <<= 8;  /* shift left for 16x16 glyph */
-               rowout >>= bbxxoff;
-               fprintf (stdout, "%04X", rowout);
-               digitsout += 4;
-            }
+				/* Force everything to 16 pixels wide */
+				for (i = 16; i > startrow; i--) {
+					fprintf(stdout, "0000");
+					digitsout += 4;
+				}
+				while (fgets(inbuf, MAXBUF - 1, stdin) != NULL && strncmp(inbuf, "END", 3) != 0) {
+					/* copy bitmap until END */
+					sscanf(inbuf, "%X", &rowout);
+					/* Now force glyph to a 16x16 grid even if they'd fit in 8x16 */
+					if (bbxx <= 8) {
+						/* shift left for 16x16 glyph */
+						rowout <<= 8;
+					}
+					rowout >>= bbxxoff;
+					fprintf(stdout, "%04X", rowout);
+					digitsout += 4;
+				}
 
-            /* Pad for 16x16 glyph */
-            while (digitsout < 64) {
-               fprintf (stdout,"0000");
-               digitsout += 4;
-            }
-            fprintf (stdout,"\n");
-         }
-      }
-   }
-   exit (0);
+				/* Pad for 16x16 glyph */
+				while (digitsout < 64) {
+					fprintf(stdout, "0000");
+					digitsout += 4;
+				}
+				fprintf(stdout, "\n");
+			}
+		}
+	}
+	exit(0);
 }
