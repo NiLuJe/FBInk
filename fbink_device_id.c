53bd1a2cdc175f1607dbdcf4549745c40acd59a6
NiLuJe <ninuje@gmail.com>
2020-09-19 00:49:07 +0200

    To that effect, allow bypassing the InkView codepath via env var

--- fbink_device_id.c
+++ fbink_device_id.c
@@ -971,28 +971,30 @@ static void
 	char* model_name = NULL;
 	// NOTE: I'm not super happy about calling InkView here,
 	//       so do it in a slightly roundabout way to try to prevent it from wreaking too much havoc...
-	void* inkview = dlopen("libinkview.so", RTLD_LAZY | RTLD_LOCAL);
-	if (!inkview) {
-		ELOG("Failed to load InkView: %s", dlerror());
-	} else {
-		dlerror();
-
-		// Try to grab the adress for InkView's GetDeviceModel function...
-		char* (*inkview_GetDeviceModel)(void) = NULL;
-		inkview_GetDeviceModel                = dlsym(inkview, "GetDeviceModel");
-
-		char* err = dlerror();
-		if (error != NULL) {
-			ELOG("Failed to obtain GetDeviceModel symbol: %s", err);
+	if (!getenv("FBINK_NO_INKVIEW")) {
+		void* inkview = dlopen("libinkview.so", RTLD_LAZY | RTLD_LOCAL);
+		if (!inkview) {
+			ELOG("Failed to load InkView: %s", dlerror());
 		} else {
-			// NOTE: We may be leaking the string here, since I have no idea what InkView does...
-			char* model = (*inkview_GetDeviceModel)();
-			//       Which is why we make a copy, just in case...
-			model_name = strdupa(model);
-		}
+			dlerror();
 
-		// Bye InkView! Hopefully your crappy dependencies haven't wreaked too much havoc...
-		dlclose(inkview);
+			// Try to grab the adress for InkView's GetDeviceModel function...
+			char* (*inkview_GetDeviceModel)(void) = NULL;
+			inkview_GetDeviceModel                = dlsym(inkview, "GetDeviceModel");
+
+			char* err = dlerror();
+			if (error != NULL) {
+				ELOG("Failed to obtain GetDeviceModel symbol: %s", err);
+			} else {
+				// NOTE: We may be leaking the string here, since I have no idea what InkView does...
+				char* model = (*inkview_GetDeviceModel)();
+				//       Which is why we make a copy, just in case...
+				model_name = strdupa(model);
+			}
+
+			// Bye InkView! Hopefully your crappy dependencies haven't wreaked too much havoc...
+			dlclose(inkview);
+		}
 	}
 
 	if (model_name) {
