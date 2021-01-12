ca38ad859e24b7c8baa7bb5eb33ec951983665e3
NiLuJe <ninuje@gmail.com>
2021-01-12 18:19:43 +0100

    Add some moire data
    c.f., the rM header for more timing details.

--- fbink.h
+++ fbink.h
@@ -205,19 +205,21 @@ typedef enum
 	//                   This is the default.
 	//                   If you request a flashing update w/ AUTO, FBInk automatically uses GC16 instead.
 	// Common
-	WFM_DU,      // From any to B&W, fast. On-screen pixels will be left as-is for new content that is *not* B&W.
+	WFM_DU,      // From any to B&W, fast (~260ms), some light ghosting.
+	//              On-screen pixels will be left as-is for new content that is *not* B&W.
 	//              Great for UI highlights, or tracing touch/pen input.
 	//              Will never flash.
 	//              DU stands for "Direct Update".
-	WFM_GC16,    // From any to any, high fidelity.
+	WFM_GC16,    // From any to any, ~450ms, high fidelity (i.e., lowest risk of ghosting).
 	//              Ideal for image content.
 	//              If flashing, will flash and update the full region.
 	//              If not, only changed pixels will update.
 	//              GC stands for "Grayscale Clearing"
-	WFM_GC4,    // From any to B/W/GRAYA/GRAY5. (may be implemented as DU4 on some devices).
+	WFM_GC4,    // From any to B/W/GRAYA/GRAY5, (~290ms), some ghosting. (may be implemented as DU4 on some devices).
 	//             Will *probably* never flash, especially if the device doesn't implement any other 4 color modes.
 	//             Limited use-cases in practice.
-	WFM_A2,     // From B&W to B&W, fast. On-screen pixels will be left as-is for new content that is *not* B&W.
+	WFM_A2,     // From B&W to B&W, fast (~120ms), some ghosting.
+	//             On-screen pixels will be left as-is for new content that is *not* B&W.
 	//             FBInk will ask the EPDC to enforce quantization to B&W to honor the "to" requirement,
 	//             (via EPDC_FLAG_FORCE_MONOCHROME).
 	//             Will never flash.
@@ -225,15 +227,15 @@ typedef enum
 	//             so as to honor the "from" requirement:
 	//             non-flashing GC16 for the in transition, A2 or GC16 for the out transition.
 	//             A stands for "Animation"
-	WFM_GL16,    // From white to any.
+	WFM_GL16,    // From white to any, ~450ms, some ghosting.
 	//              Typically optimized for text on a white background.
 	// Newer generation devices only
-	WFM_REAGL,    // From white to any, with ghosting and flashing reduction.
+	WFM_REAGL,    // From white to any, ~450ms, with ghosting and flashing reduction.
 	//               When available, best option for text (in place of GL16).
 	//               May enforce timing constraints if in collision with another waveform mode, e.g.,
 	//               it may, to some extent, wait for completion of previous updates to have access to HW resources.
 	//               Marketing term for the feature is "Regal". Technically called 5-bit waveform modes.
-	WFM_REAGLD,    // From white to any, with more ghosting reduction, but less flashing reduction.
+	WFM_REAGLD,    // From white to any, ~450ms, with more ghosting reduction, but less flashing reduction.
 	//                Should only be used when flashing, which should yield a less noticeable flash than GC16.
 	//                Rarely used in practice, because still optimized for text or lightly mixed content,
 	//                not pure image content.
@@ -246,7 +248,7 @@ typedef enum
 	WFM_GCK16,        // From black to any. Goes hand-in-hand with GLKW16, should only be used when flashing.
 	WFM_GLKW16,       // From black to any. Newer variant of GL16_INV.
 	// For documentation purposes
-	WFM_INIT,    // May flash several times to end up with a white screen.
+	WFM_INIT,    // May flash several times to end up with a white screen, slow (~2000ms).
 	WFM_UNKNOWN,
 	// reMarkable only
 	WFM_INIT2,
