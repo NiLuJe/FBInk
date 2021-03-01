4351df033b0dcba11564d0523eb261dd7ded6450
NiLuJe <ninuje@gmail.com>
2021-03-01 04:51:49 +0100

    Hmm, reading through the lines on current kernels, A2 may also not be a given...

--- fbink.h
+++ fbink.h
@@ -198,7 +198,7 @@ typedef uint8_t BG_COLOR_INDEX_T;
 //       This is currently left at your own discretion, though.
 //       c.f., https://github.com/NiLuJe/FBInk/commit/32acece78f7cc92b06faa4a668feead260b8ce24
 // NOTE: On very old devices (e.g., Kobo Mk. 3 & 4; possibly early PB), only AUTO, DU & GC16 may be relied on.
-//       A2 & GC4 will probably behave, too (especially A2), but GL16 is not a given at all:
+//       GC4 will probably behave, but A2 & GL16 are not a given at all:
 //       e.g., it's actively broken on Kobo <= Mk. 4: c.f., https://github.com/baskerville/plato/issues/158#issuecomment-787520759.
 //       If a waveform mode produces unexpected/broken results, and/or if you start to hit unexpected EPDC timeouts (or even an OOPS),
 //       that's usually a strong hint that you're trying to use something you shouldn't ;).
