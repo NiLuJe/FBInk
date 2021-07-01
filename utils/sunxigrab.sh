0af185184f0cb567438f1db18aafd3ffd6e01c2e
NiLuJe <ninuje@gmail.com>
2021-07-01 19:24:57 +0200

    Write a stupid wrapper around get_working_buffer_data ...
    Because we can't rely on fb0 anymore :(

---
+++ utils/sunxigrab.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Since every process writes into a private dmabuff instead of the fb's memory,
+# we can't really rely on just dumping the fb memory region...
+# Instead, rely on a disp sysfs attributes that dumps the working buffer to a BMP...
+
+# The path is hardcoded by the kernel, so, make it a tmpfs...
+SUNXI_PATH="/mnt/flash"
+mkdir -p "${SUNXI_PATH}"
+if ! grep -q "^tmpfs ${SUNXI_PATH} tmpfs " /proc/mounts ; then
+	# Panel is 1404x1872, so we'll need a bit over 2.5M
+	mount -t tmpfs tmpfs ${SUNXI_PATH} -o noatime,size=3M
+fi
+
+# Do the thing!
+cat /sys/devices/virtual/disp/disp/waveform/get_working_buffer > /dev/null
+
+echo "Working buffer dumped to ${SUNXI_PATH}/workingbuffer.bmp"
