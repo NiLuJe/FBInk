3b4799dccda568a40ffa911b2bf6091b535e3a3a
NiLuJe <ninuje@gmail.com>
2021-07-16 23:32:03 +0200

    Init ion.fd to -1

--- fbink_internal.h
+++ fbink_internal.h
@@ -448,7 +448,7 @@ FBInkKoboSunxi sunxiCtx = { .disp_fd    = -1,
 			    .i2c_dev    = { 0 },
 			    .ion_fd     = -1,
 			    .alloc_size = 0U,
-			    .ion        = { 0 },
+			    .ion        = { .handle = 0, .fd = -1 },
 			    .layer      = { { 0 } },
 			    .rota       = 0U,
 			    .no_rota    = false };
