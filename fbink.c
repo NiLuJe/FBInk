9e6889f8ffc4df64716c519447439d2214065785
NiLuJe <ninuje@gmail.com>
2022-02-07 23:06:10 +0100

    Forgot the new mystery struct

--- fbink.c
+++ fbink.c
@@ -2136,6 +2136,7 @@ static int
 				   : (waveform_mode == MTK_WAVEFORM_MODE_GL4 || waveform_mode == MTK_WAVEFORM_MODE_DU4) ? 3
 															: 7,
 		.alt_buffer_data = { 0U },
+		.swipe_data      = { 0U },
 		.hist_bw_waveform_mode =
 		    (waveform_mode == MTK_WAVEFORM_MODE_REAGL) ? MTK_WAVEFORM_MODE_REAGL : MTK_WAVEFORM_MODE_DU,
 		.hist_gray_waveform_mode =
