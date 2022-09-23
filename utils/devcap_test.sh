0a627d6d65c194195451455771e3c08d78deb758
NiLuJe <ninuje@gmail.com>
2022-09-23 20:38:55 +0200

    DevCap: Junk paths in the tarball

--- utils/devcap_test.sh
+++ utils/devcap_test.sh
@@ -163,5 +163,5 @@ fbgrab "${DEVCAP_PIC}" >/dev/null 2>&1
 echo "Screengrab saved to ${DEVCAP_PIC}"
 
 # Tar it up in the PWD, too
-tar -cvzf Kobo-DevCap-Results.tar.gz "${DEVCAP_LOG}" "${DEVCAP_PIC}"
+tar -C /mnt/onboard -cvzf ${PWD}/Kobo-DevCap-Results.tar.gz "${DEVCAP_LOG##*/}" "${DEVCAP_PIC##*/}"
 echo "Results compiled in Kobo-DevCap-Results.tar.gz"
