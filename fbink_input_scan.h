365b119a71f0c5739d9971028bde32f5808a5767
NiLuJe <ninuje@gmail.com>
2024-05-12 19:22:06 +0200

    Document the custom NTX input event codes while I'm in there...

--- fbink_input_scan.h
+++ fbink_input_scan.h
@@ -123,17 +123,35 @@ static __attribute__((cold)) void        concat_type_recap(INPUT_DEVICE_TYPE_T t
 // Each of our target platforms tend to settle on some specific,
 // sometimes barely related, keycodes for common functions...
 #	if defined(FBINK_FOR_KOBO)
-#		define PLATFORM_KEY_POWER        KEY_POWER
-#		define PLATFORM_KEY_SLEEP        KEY_H     // PowerCover
-#		define PLATFORM_KEY_WAKEUP       KEY_F1    // Also PowerCover, depending on the device...
-#		define PLATFORM_KEY_PGPREV       KEY_F23
-#		define PLATFORM_KEY_PGNEXT       KEY_F24
-#		define PLATFORM_KEY_HOME         KEY_HOME
-#		define PLATFORM_KEY_FRONTLIGHT   KEY_KATAKANA
-#		define PLATFORM_KEY_MENU         0
-#		define PLATFORM_KEY_DPAD         0
-#		define PLATFORM_ROTATION_EV_TYPE EV_MSC
-#		define PLATFORM_ROTATION_EV_CODE MSC_RAW
+#		define PLATFORM_KEY_POWER              KEY_POWER
+#		define PLATFORM_KEY_SLEEP              KEY_H     // PowerCover
+#		define PLATFORM_KEY_WAKEUP             KEY_F1    // Also PowerCover, depending on the device...
+#		define PLATFORM_KEY_PGPREV             KEY_F23
+#		define PLATFORM_KEY_PGNEXT             KEY_F24
+#		define PLATFORM_KEY_HOME               KEY_HOME
+#		define PLATFORM_KEY_FRONTLIGHT         KEY_KATAKANA
+#		define PLATFORM_KEY_MENU               0
+#		define PLATFORM_KEY_DPAD               0
+#		define PLATFORM_ROTATION_EV_TYPE       EV_MSC
+#		define PLATFORM_ROTATION_EV_CODE       MSC_RAW
+// For reference, custom NTX input-event-codes we might care about:
+// Pen stuff
+/*
+#		define ABS_AZIMUTH                     0x1d
+#		define ABS_TIP_MAX                     0x29
+#		define ABS_TIP_MIN                     0x2a
+#		define ABS_TIP_HIST_MIN                0x2b
+#		define ABS_TIP_HIST_MAX                0x2c
+*/
+// ROTATION_EVENT values
+/*
+#		define MSC_RAW_GSENSOR_PORTRAIT_DOWN   0x17
+#		define MSC_RAW_GSENSOR_PORTRAIT_UP     0x18
+#		define MSC_RAW_GSENSOR_LANDSCAPE_RIGHT 0x19
+#		define MSC_RAW_GSENSOR_LANDSCAPE_LEFT  0x1a
+#		define MSC_RAW_GSENSOR_BACK            0x1b
+#		define MSC_RAW_GSENSOR_FRONT           0x1c
+*/
 #	elif defined(FBINK_FOR_KINDLE)
 #		define PLATFORM_KEY_POWER        KEY_POWER    // Generally handled by powerd
 #		define PLATFORM_KEY_SLEEP        0            // :?
